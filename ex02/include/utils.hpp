51a03d6f34bd1864661a5cf9f11008d1bf59328d
Jay Takahashi <qsakura14555@gmail.com>
2025-05-28 22:11:15 +0900

    feat(SRC): ✨  add utils.[hpp, cpp]

---
+++ ex02/include/utils.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <deque>
+#include <iostream>
+#include <string>
+#include <vector>
+
+void printVector(const std::vector<int>& vectorNumbers);
+void printDeque(const std::deque<int>& dequeNumbers);
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -5,33 +5,24 @@
 #include <vector>
 
 #include "color.hpp"
+#include "utils.hpp"
 
 static bool isValidArgument(const std::string& arg,
                             std::vector<int>& vectorNumbers,
                             std::deque<int>& dequeNumbers) {
-  for (size_t i = 0; i < arg.length(); ++i) {
-    if (isspace(static_cast<unsigned char>(arg[i]))) {
-      continue;
-    }
-    char* endPtr;
-    if (isdigit(static_cast<unsigned char>(arg[i]))) {
-      long number = std::strtol(&arg[i], &endPtr, 10);
-      if (endPtr == &arg[i] || number < 0 || number > INT_MAX) {
-        return false;
-      } else {
-        vectorNumbers.push_back(static_cast<int>(number));
-        dequeNumbers.push_back(static_cast<int>(number));
-        i = endPtr - arg.c_str() - 1;
-      }
-    } else {
-      return false;
-    }
+  char* endPtr;
+  long number = std::strtol(arg.c_str(), &endPtr, 10);
+  if (*endPtr != '\0') {
+    return false;
+  } else {
+    vectorNumbers.push_back(static_cast<int>(number));
+    dequeNumbers.push_back(static_cast<int>(number));
   }
   return true;
 }
 
 int main(int argc, char** argv) {
-  if (argc != 2) {
+  if (argc == 1) {
     std::cerr << RED << "Error: invalid number of arguments." << RESET
               << std::endl;
     return EXIT_FAILURE;
@@ -40,16 +31,19 @@ int main(int argc, char** argv) {
   std::vector<int> vectorNumbers;
   std::deque<int> dequeNumbers;
 
-  if (!isValidArgument(argv[1], vectorNumbers, dequeNumbers)) {
-    std::cerr << RED << "Error: invalid argument format." << RESET << std::endl;
-    return EXIT_FAILURE;
+  for (int i = 1; i < argc; ++i) {
+    if (!isValidArgument(argv[i], vectorNumbers, dequeNumbers)) {
+      std::cerr << RED << "Error: invalid argument '" << argv[i] << "'."
+                << RESET << std::endl;
+      return EXIT_FAILURE;
+    }
   }
 
-  for (std::vector<int>::const_iterator it = vectorNumbers.begin();
-       it != vectorNumbers.end(); ++it) {
-    std::cout << *it << " ";
-  }
-  std::cout << std::endl;
+  std::cout << "Before:  ";
+  printVector(vectorNumbers);
+
+  std::cout << "After:   ";
+  printDeque(dequeNumbers);
 
   return EXIT_SUCCESS;
 }
